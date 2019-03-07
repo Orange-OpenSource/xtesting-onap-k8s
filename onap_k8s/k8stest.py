d3cc8076daf772ece2567885db09a7d54759619d
mrichomme <morgan.richomme@orange.com>
2019-03-07 12:03:04 +0100

    Add python class for the traces
    Signed-off-by: mrichomme <morgan.richomme@orange.com>

---
+++ onap_k8s/k8stest.py
@@ -0,0 +1,130 @@
+#!/usr/bin/env python
+#
+# Copyright (c) 2018 All rights reserved
+# This program and the accompanying materials
+# are made available under the terms of the Apache License, Version 2.0
+# which accompanies this distribution, and is available at
+#
+# http://www.apache.org/licenses/LICENSE-2.0
+#
+
+"""
+Define the parent for Kubernetes testing.
+"""
+
+from __future__ import division
+
+import logging
+import os
+import re
+import subprocess
+import time
+
+from xtesting.core import testcase
+
+
+class K8sTesting(testcase.TestCase):
+    """Kubernetes test runner"""
+
+    __logger = logging.getLogger(__name__)
+
+    config = '/conf/.kube/config'
+
+    def __init__(self, **kwargs):
+        super(K8sTesting, self).__init__(**kwargs)
+        self.cmd = []
+        self.result = 0
+        self.start_time = 0
+        self.stop_time = 0
+
+    def run_kubetest(self):  # pylint: disable=too-many-branches
+        """Run the test suites"""
+        cmd_line = self.cmd
+        self.__logger.info("Starting k8s test: '%s'.", cmd_line)
+
+        process = subprocess.Popen(cmd_line, stdout=subprocess.PIPE,
+                                   stderr=subprocess.STDOUT)
+        output = process.stdout.read()
+        if ('Error loading client' in output or
+                'Unexpected error' in output):
+            raise Exception(output)
+
+        remarks = []
+        lines = output.split('\n')
+        success = False
+        failure = False
+        i = 0
+        while i < len(lines):
+            if '[Fail]' in lines[i] or 'Failures:' in lines[i]:
+                self.__logger.error(lines[i])
+            if re.search(r'\[(.)*[0-9]+ seconds\]', lines[i]):
+                self.__logger.debug(lines[i])
+                i = i + 1
+                while i < len(lines) and lines[i] != '-' * len(lines[i]):
+                    if lines[i].startswith('STEP:') or ('INFO:' in lines[i]):
+                        break
+                    self.__logger.debug(lines[i])
+                    i = i + 1
+            if i >= len(lines):
+                break
+            success = 'SUCCESS!' in lines[i]
+            failure = 'FAIL!' in lines[i]
+            if success or failure:
+                if i != 0 and 'seconds' in lines[i - 1]:
+                    remarks.append(lines[i - 1])
+                remarks = remarks + lines[i].replace('--', '|').split('|')
+                break
+            i = i + 1
+
+        self.__logger.debug('-' * 10)
+        self.__logger.info("Remarks:")
+        for remark in remarks:
+            if 'seconds' in remark:
+                self.__logger.debug(remark)
+            elif 'Passed' in remark:
+                self.__logger.info("Passed: %s", remark.split()[0])
+            elif 'Skipped' in remark:
+                self.__logger.info("Skipped: %s", remark.split()[0])
+            elif 'Failed' in remark:
+                self.__logger.info("Failed: %s", remark.split()[0])
+
+        if success:
+            self.result = 100
+        elif failure:
+            self.result = 0
+
+    def run(self, **kwargs):
+
+        if not os.path.isfile(self.config):
+            self.__logger.error(
+                "Cannot run k8s testcases. Config file not found")
+            return self.EX_RUN_ERROR
+
+        self.start_time = time.time()
+        try:
+            self.run_kubetest()
+            res = self.EX_OK
+        except Exception:  # pylint: disable=broad-except
+            self.__logger.exception("Error with running kubetest:")
+            res = self.EX_RUN_ERROR
+
+        self.stop_time = time.time()
+        return res
+
+
+class K8sOnapTest(K8sTesting):
+    """Kubernetes smoke test suite"""
+    def __init__(self, **kwargs):
+        if "case_name" not in kwargs:
+            kwargs.get("case_name", 'k8s_smoke')
+        super(K8sOnapTest, self).__init__(**kwargs)
+        self.cmd = ['/check_onap_k8s.sh']
+
+
+class K8sHelmTest(K8sTesting):
+    """Kubernetes conformance test suite"""
+    def __init__(self, **kwargs):
+        if "case_name" not in kwargs:
+            kwargs.get("case_name", 'k8s_conformance')
+        super(K8sHelmTest, self).__init__(**kwargs)
+        self.cmd = ['/check_onap_helm.sh']
---
+++ onap_k8s/test_k8stest.py
@@ -0,0 +1,78 @@
+#!/usr/bin/env python
+#
+# Copyright (c) 2018 All rights reserved
+# This program and the accompanying materials
+# are made available under the terms of the Apache License, Version 2.0
+# which accompanies this distribution, and is available at
+#
+# http://www.apache.org/licenses/LICENSE-2.0
+#
+
+"""Define the classes required to fully cover k8s."""
+
+import logging
+import os
+import unittest
+
+import mock
+from xtesting.core import testcase
+
+from onap_k8s import k8stest
+
+
+class K8sTests(unittest.TestCase):
+
+    # pylint: disable=missing-docstring
+
+    def setUp(self):
+        os.environ["DEPLOY_SCENARIO"] = "k8-test"
+        os.environ["KUBE_MASTER_IP"] = "127.0.0.1"
+        os.environ["KUBE_MASTER_URL"] = "https://127.0.0.1:6443"
+        os.environ["KUBERNETES_PROVIDER"] = "local"
+
+        self.k8stesting = k8stest.K8sTesting()
+
+    @mock.patch('onap_k8s.k8stest.os.path.isfile',
+                return_value=False)
+    def test_run_missing_config_file(self, mock_func):
+        self.k8stesting.config = 'not_file'
+        with mock.patch.object(self.k8stesting,
+                               '_K8sTesting__logger') as mock_logger:
+            self.assertEquals(self.k8stesting.run(),
+                              testcase.TestCase.EX_RUN_ERROR)
+            mock_logger.error.assert_called_with(
+                "Cannot run k8s testcases. Config file not found")
+        mock_func.assert_called_with('not_file')
+
+    def test_run_kubetest_cmd_none(self):
+        self.k8stesting.cmd = None
+        with self.assertRaises(TypeError):
+            self.k8stesting.run_kubetest()
+
+    @mock.patch('onap_k8s.k8stest.os.path.isfile')
+    def test_error_logging(self, mock_isfile):
+        # pylint: disable=unused-argument
+        with mock.patch('onap_k8s.k8stest.'
+                        'subprocess.Popen') as mock_popen, \
+             mock.patch.object(self.k8stesting,
+                               '_K8sTesting__logger') as mock_logger:
+            mock_stdout = mock.Mock()
+            attrs = {'stdout.read.return_value': 'Error loading client'}
+            mock_stdout.configure_mock(**attrs)
+            mock_popen.return_value = mock_stdout
+            self.k8stesting.run()
+            mock_logger.exception.assert_called_with(
+                "Error with running kubetest:")
+
+    @mock.patch('onap_k8s.k8stest.os.path.isfile')
+    @mock.patch('onap_k8s.k8stest.subprocess.Popen')
+    def test_run(self, mock_open, mock_isfile):
+        self.assertEquals(self.k8stesting.run(),
+                          testcase.TestCase.EX_OK)
+        mock_isfile.assert_called()
+        mock_open.assert_called()
+
+
+if __name__ == "__main__":
+    logging.disable(logging.CRITICAL)
+    unittest.main(verbosity=2)
